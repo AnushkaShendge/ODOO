12295b0e7c3543413a7b4b47d4106e0c4af64969
HARSHITBHANUSHALI <harshit.bhanushali22@spit.ac.in>
2025-03-14 06:15:41 +0530

    sockets backend

---
+++ backend/Sockets/index.js
@@ -0,0 +1,47 @@
+let users = {}; // Stores userId -> socketId mapping
+
+const initializeSocket = (io) => {
+    io.on("connection", (socket) => {
+        console.log(`User connected: ${socket.id}`);
+
+        // User joins their own room
+        socket.on("joinRoom", (username) => {
+            socket.join(username);
+            console.log(`User ${username} joined room ${username}`);
+        });
+
+        // Receive location updates and send them to friends' rooms
+        socket.on("shareLocation", async (data) => {
+            const { username, latitude, longitude } = data;
+
+            // Fetch friends from backend (Assume DB call)
+            const friends = await getFriendsFromDatabase(username);
+
+            // Emit location update only to friends' rooms
+            friends.forEach((friendUsername) => {
+                io.to(friendUsername).emit("locationUpdate", { username, latitude, longitude });
+            });
+
+            console.log(`Shared location of ${username} with ${friends}`);
+        });
+
+        // Handle disconnect
+        socket.on("disconnect", () => {
+            const userId = Object.keys(users).find((key) => users[key] === socket.id);
+            if (userId) {
+                delete users[userId];
+                console.log(`User ${userId} disconnected`);
+            }
+        });
+    });
+
+    return io;
+};
+
+// Mock function to fetch friends (Replace with DB call)
+const getFriendsFromDatabase = async (username) => {
+    // Assume we have a database call that fetches friend IDs of the user
+    return ["friend1", "friend2", "friend3"]; // Replace with actual friend IDs
+};
+
+module.exports = { initializeSocket };
